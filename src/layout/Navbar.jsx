d9be429266a0e9af51e44ee8d03e9262577b3d3d
YeChan Kim <az20058@naver.com>
2024-12-27 20:51:14 +0900

    🎉Start : 프로젝트 시작

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
