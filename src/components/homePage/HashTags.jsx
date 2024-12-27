5a5a946d29fefcb0fcf619df42fa371d5bbbf1a7
YeChan Kim <az20058@naver.com>
2024-12-27 22:18:50 +0900

    Feat : 네브바 제작

---
+++ src/components/homePage/HashTags.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export default function HashTags() {
+  return (
+    <Wrapper>
+      <h2>사업 이름</h2>
+      <HashTag></HashTag>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  border: 1px solid #fff5a0;
+
+  h2 {
+    font-size: 15px;
+    font-weight: 600;
+    color: white;
+  }
+`;
+
+const HashTag = styled.div``;
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,13 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
 export default function Navbar() {
+  const navigate = useNavigate();
+
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Wrapper>
+      <Nav>
+        <Logo onClick={() => navigate('/')}>
+          <img src="/images/logo.png" alt="logo" />
+          <h1>EMBER</h1>
+        </Logo>
+        <img src="/images/user.png" alt="user" />
+      </Nav>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.header`
+  height: 72px;
+  width: 100%;
+  background-color: black;
+  padding: 16px 20px;
+  box-sizing: border-box;
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  h1 {
+    margin-top: 10px;
+    margin-left: 8px;
+    font-weight: 600;
+    font-size: 25px;
+    color: white;
+  }
+`;
